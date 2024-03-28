c580c68e07fbd78d8532f5070edd8d31a82b241a
DanielZhong24 <danielzhong0618@gmail.com>
2024-03-28 12:53:01 -0400

    register

---
+++ src/loginsystem/User.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package loginsystem;
+
+/**
+ *
+ * @author Daniel Zhong
+ */
+public class User {
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private String email;
+
+    public User(String[] newUser) {
+        this.firstName = newUser[0];
+        this.lastName = newUser[1];
+        this.userName = newUser[2];
+        this.password = newUser[3];
+        this.email = newUser[4];
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+    
+    
+}
