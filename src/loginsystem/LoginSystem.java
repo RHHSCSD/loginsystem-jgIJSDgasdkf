9d6a7685fba880d2110b12686a44720a66c0ad2d
DanielZhong24 <danielzhong0618@gmail.com>
2024-04-01 17:22:00 -0400

    register

--- src/loginsystem/LoginSystem.java
+++ src/loginsystem/LoginSystem.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package loginsystem;
-
+import java.io.*;
 /**
  *
  * @author michael.roy-diclemen
@@ -15,6 +15,16 @@ public class LoginSystem {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        try{
+            RegistrationSystem r = new RegistrationSystem("src/loginsystem/user.txt");
+//            System.out.println(r.getUsers());
+
+        }catch(FileNotFoundException e){
+            System.out.println("File not found you noob");
+        }
+        
+        
     }
     
 }
